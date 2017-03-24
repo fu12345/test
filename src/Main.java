f183784b2a481df2b23840f0d496c9ade321b56a
fu12345 <1160201700@qq.com>
2017-03-24 19:18:22 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.client.SelectOptimalEncodingFeature;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Wd!");
+        
+    }
+}
